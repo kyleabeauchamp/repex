f6847b3816e5346ac0085179b59dd6e100999da6
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-12-05 14:36:45 -0500

    Cleanup in test.

--- tests/test_harmonic_oscillators_serial.py
+++ tests/test_harmonic_oscillators_serial.py
@@ -5,6 +5,22 @@ from repex.replica_exchange import ReplicaExchange
 from repex import testsystems
 import tempfile
 from mdtraj.testing import eq
+from mdtraj.utils import ensure_type
+
+def permute_energies(u0, s):
+    """Re-order an observable u0 so that u[i, j, k] correponds to frame i, sampled from state j, evaluated in state k."""
+    u0 = ensure_type(u0, 'float32', 3, "u0")
+    n_iter, n_replicas, n_replicas = u0.shape
+    s = ensure_type(s, "int", 2, "s", shape=(n_iter, n_replicas))
+    
+    u = np.zeros((n_iter, n_replicas, n_replicas))
+    for i, si in enumerate(s):
+        mapping = dict(zip(range(n_replicas), si))
+        inv_map = {v:k for k, v in mapping.items()}
+        si_inv = [inv_map[k] for k in range(n_replicas)]
+        u[i] = u0[i, si_inv]
+    
+    return u
 
 def test_harmonic_oscillators():
     """Test harmonic oscillator reduced potentials at temperatures 1K, 10K, 100K."""
@@ -34,16 +50,8 @@ def test_harmonic_oscillators():
     u_permuted = replica_exchange.database.ncfile.variables["energies"][:]
     s = replica_exchange.database.ncfile.variables["states"][:]
 
-    u = np.array([u_permuted[i, s[i]] for i in range(len(s))])
-    
-    u = np.zeros((len(s), n_replicas, n_replicas))
-    for i, si in enumerate(s):
-        mapping = dict(zip(range(n_replicas), si))
-        inv_map = {v:k for k, v in mapping.items()}
-        si_inv = [inv_map[k] for k in range(n_replicas)]
-        u[i] = u_permuted[i, si_inv]
-    
-    
+    u = permute_energies(u_permuted, s)
+
     u0 = np.array([[ho.get_reduced_potential_expectation(s0, s1) for s1 in states] for s0 in states])
 
     l0 = np.log(u0)  # Compare on log scale because uncertainties are proportional to values
