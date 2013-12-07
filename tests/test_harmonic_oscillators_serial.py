f29db05ee17715aa6c276c8dccc0bb558c328f20
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2013-12-06 19:27:34 -0500

    Added simple tests for save and load functionality.

--- tests/test_harmonic_oscillators_serial.py
+++ tests/test_harmonic_oscillators_serial.py
@@ -44,3 +44,30 @@ def test_harmonic_oscillators():
     l1 = np.log(u.mean(0))
     eq(l0, l1, decimal=1)
 
+
+def test_harmonic_oscillators_save_and_load():
+
+    nc_filename = tempfile.mkdtemp() + "/out.nc"
+
+    T_min = 1.0 * unit.kelvin
+    T_i = [T_min, T_min * 10., T_min * 100.]
+    n_replicas = len(T_i)
+
+    ho = testsystems.HarmonicOscillator()
+
+    system = ho.system
+    positions = ho.positions
+
+    states = [ ThermodynamicState(system=system, temperature=T_i[i]) for i in range(n_replicas) ]
+
+    coordinates = [positions] * n_replicas
+
+    replica_exchange = ReplicaExchange.create_repex(states, coordinates, nc_filename, **{})
+    replica_exchange.number_of_iterations = 200
+    replica_exchange.run()
+    
+    
+    replica_exchange = ReplicaExchange.resume_repex(nc_filename)
+    eq(replica_exchange.iteration, 200)
+    replica_exchange.number_of_iterations = 300
+    replica_exchange.run()
--- tests/test_power_oscillators_hamiltonian_exchange_serial.py
+++ tests/test_power_oscillators_hamiltonian_exchange_serial.py
@@ -43,3 +43,31 @@ def test_power_oscillators():
     l0 = np.log(u0)
 
     eq(l0, l, decimal=1)
+
+def test_hrex_save_and_load():
+
+    nc_filename = tempfile.mkdtemp() + "/out.nc"
+
+    temperature = 1 * unit.kelvin
+
+    K0 = 100.0  # Units are automatically added by the testsystem
+    K = [K0, K0 * 10., K0 * 1.]
+    powers = [2., 2., 4.]
+    n_replicas = len(K)
+
+    oscillators = [testsystems.PowerOscillator(b=powers[i]) for i in range(n_replicas)]
+
+    systems = [ho.system for ho in oscillators]
+    positions = [ho.positions for ho in oscillators]
+
+    state = ThermodynamicState(system=systems[0], temperature=temperature)
+
+    replica_exchange = hamiltonian_exchange.HamiltonianExchange.create_repex(state, systems, positions, nc_filename, **{})
+    replica_exchange.number_of_iterations = 200
+    replica_exchange.run()
+
+    
+    replica_exchange = hamiltonian_exchange.HamiltonianExchange.resume_repex(nc_filename)
+    eq(replica_exchange.iteration, 200)
+    replica_exchange.number_of_iterations = 300
+    replica_exchange.run()
