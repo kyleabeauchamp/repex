f3e0b73b50c25e906be9426c93749d175556a7e5
John Chodera <choderaj@mskcc.org>
2013-12-25 11:34:57 -0800

    Updated nose test of MCMC to test both volume and potential energy expectations. Still only test one combination of MCMC moves.

--- tests-nose/test_mcmc.py
+++ tests-nose/test_mcmc.py
@@ -9,7 +9,11 @@ from pymbar import timeseries
 
 analytical_testsystems = ["HarmonicOscillatorArray", "IdealGas"] # systems with analytical results to test
 
+platform_name = "CUDA" # platform to use
+
 NSIGMA_CUTOFF = 6.0 # cutoff for significance testing
+
+debug = False # set to True only for manual debugging of this nose test
 
 def test_mcmc_expectations():
     # Select system:
@@ -19,12 +23,13 @@ def test_mcmc_expectations():
         test_mcmc_expectation(testsystem)
 
 def test_mcmc_expectation(testsystem):
+    if debug: print testsystem.__class__.__name__
+
     # Test settings.
     temperature = 298.0 * units.kelvin
     pressure = 1.0 * units.atmospheres
     nequil = 10 # number of equilibration iterations
     niterations = 100 # number of production iterations
-    platform_name = "CUDA" # platform to try
 
     # Retrieve system and positions.
     [system, positions] = [testsystem.system, testsystem.positions]
@@ -65,8 +70,9 @@ def test_mcmc_expectation(testsystem):
     potential_n = numpy.zeros([niterations], numpy.float64) # potential_n[i] is the potential energy after iteration i, in kT
     kinetic_n = numpy.zeros([niterations], numpy.float64) # kinetic_n[i] is the kinetic energy after iteration i, in kT
     temperature_n = numpy.zeros([niterations], numpy.float64) # temperature_n[i] is the instantaneous kinetic temperature from iteration i, in K
+    volume_n = numpy.zeros([niterations], numpy.float64) # volume_n[i] is the volume from iteration i, in K
     for iteration in range(niterations):
-        #print "iteration %d / %d" % (iteration, niterations)
+        if debug: print "iteration %d / %d" % (iteration, niterations)
 
         # Update sampler state.
         sampler_state = sampler.run(sampler_state, 1)
@@ -85,29 +91,62 @@ def test_mcmc_expectation(testsystem):
         potential_n[iteration] = potential_energy / kT
         kinetic_n[iteration] = kinetic_energy / kT
         temperature_n[iteration] = instantaneous_temperature / units.kelvin
+        volume_n[iteration] = volume / (units.nanometers**3)
 
     # Compute expected statistics.
-    if ('potential_expectation' in dir(testsystem)):
+    if ('get_potential_expectation' in dir(testsystem)):
         # Skip this check if the std dev is zero.
         skip_test = False
-        if ('potential_standard_deviation' in dir(testsystem)):
-            potential_standard_deviation = testsystem.get_potential_standard_deviation() / kT
-            if potential_standard_deviation == 0.0:
-                skip_test = True
+        if (potential_n.std() == 0.0):
+            skip_test = True
+            if debug: print "Skipping potential test since variance is zero."
         if not skip_test:
-            potential_expectation = testsystem.get_potential_expectation() / kT
+            potential_expectation = testsystem.get_potential_expectation(thermodynamic_state) / kT
             potential_mean = potential_n.mean()            
             g = timeseries.statisticalInefficiency(potential_n, fast=True)
             dpotential_mean = potential_n.std() / numpy.sqrt(niterations / g)
             potential_error = potential_mean - potential_expectation
             nsigma = abs(potential_error) / dpotential_mean
-            # Test if error exceeds threshhold.
+            test_passed = True
             if (nsigma > NSIGMA_CUTOFF):
+                test_passed = False
+
+            if debug or (test_passed is False):
                 print "Potential energy expectation"
-                print "observed %10.5 +- %10.5f kT | expected %10.5f | error %10.5f +- %10.5f (%.1f sigma)" % (potential_mean, dpotential_mean, potential_expectation, potential_error, dpotential_mean, nsigma)
-                print "TEST FAILED"
+                print "observed %10.5f +- %10.5f kT | expected %10.5f | error %10.5f +- %10.5f (%.1f sigma)" % (potential_mean, dpotential_mean, potential_expectation, potential_error, dpotential_mean, nsigma)
+                if test_passed:
+                    print "TEST PASSED"
+                else:                
+                    print "TEST FAILED"
                 print "----------------------------------------------------------------------------"
 
+    if ('get_volume_expectation' in dir(testsystem)):
+        # Skip this check if the std dev is zero.
+        skip_test = False
+        if (volume_n.std() == 0.0):
+            skip_test = True
+            if debug: print "Skipping volume test."
+        if not skip_test:
+            volume_expectation = testsystem.get_volume_expectation(thermodynamic_state) / (units.nanometers**3)
+            volume_mean = volume_n.mean()            
+            g = timeseries.statisticalInefficiency(volume_n, fast=True)
+            dvolume_mean = volume_n.std() / numpy.sqrt(niterations / g)
+            volume_error = volume_mean - volume_expectation
+            nsigma = abs(volume_error) / dvolume_mean
+            test_passed = True
+            if (nsigma > NSIGMA_CUTOFF):
+                test_passed = False
+
+            if debug or (test_passed is False):
+                print "Volume expectation"
+                print "observed %10.5f +- %10.5f kT | expected %10.5f | error %10.5f +- %10.5f (%.1f sigma)" % (volume_mean, dvolume_mean, volume_expectation, volume_error, dvolume_mean, nsigma)
+                if test_passed:
+                    print "TEST PASSED"
+                else:                
+                    print "TEST FAILED"
+                print "----------------------------------------------------------------------------"
+
+                
 
 #=============================================================================================
 # MAIN AND TESTS
