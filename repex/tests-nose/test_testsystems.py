86597b5a5761e19168c00e6cb3b686bed57ae74e
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-01-13 17:59:03 -0500

    Repex nosetests don't need to run doctests on openmmtools.

--- repex/tests-nose/test_testsystems.py
+++ repex/tests-nose/test_testsystems.py
@@ -8,10 +8,6 @@ import tempfile
 from mdtraj.testing import eq, skipif
 import logging
 
-def test_doctest():
-    import doctest
-    from openmmtools import testsystems
-    doctest.testmod(testsystems)
 
 def test_properties_all_testsystems():
     testsystem_classes = testsystems.TestSystem.__subclasses__()
